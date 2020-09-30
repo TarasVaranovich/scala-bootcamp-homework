f78359ece14a71a01236cab3dee0e2a156289db2
Taras Varanovich <taras.varanovich@gmail.com>
2020-09-30 11:23:58 +0300

    HW1 - fixed unsuccessful flow

--- src/main/scala/basics/Basics.scala
+++ src/main/scala/basics/Basics.scala
@@ -12,12 +12,12 @@ object Basics {
    * @return least number which can be divided without reminder a and b.
    *         Throws exception in case if parameters to large
    */
-  def lcm(a: Int, b: Int): Int = {
+  def lcm(a: Int, b: Int): Either[Int, String] = {
     if (a == 0 || b == 0) {
-      throw new Exception(s"Cannot calculate lcm for $a and $b.")
+      return Right(s"Cannot calculate lcm for $a and $b.")
     }
-    if ((BigInt(a) * BigInt(b)).abs.isValidInt) {
-      throw new Exception("Result not predictable. Function doesn't work with BigInt.")
+    if (!(BigInt(a) * BigInt(b)).abs.isValidInt) {
+      return Right("Result not predictable. Function doesn't work with BigInt.")
     }
     val maxValue: Int = scala.math.max(a, b).abs
     val minValue: Int = scala.math.min(a, b).abs
@@ -26,7 +26,7 @@ object Basics {
       if (candidate % minValue == 0) candidate else lcmCalculator(minValue, increment, candidate + increment)
     }
 
-    lcmCalculator(minValue, maxValue, maxValue)
+    Left(lcmCalculator(minValue, maxValue, maxValue))
   }
 
   /**
--- src/test/scala/basics/BasicsSpec.scala
+++ src/test/scala/basics/BasicsSpec.scala
@@ -1,23 +1,24 @@
-import basics.Basics
+package basics
+
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 class BasicsSpec extends AnyFlatSpec {
 
   "LCD of 12 and 18" should "be 36" in {
-    Basics.lcm(12, 18) shouldBe 36
+    Basics.lcm(12, 18).left.getOrElse(0) shouldBe 36
   }
 
   "LCD of 10,025,005 and 9,900,044" should "throw exception" in {
-    assertThrows[Exception](Basics.lcm(10025005, 9900044))
+    assert(Basics.lcm(10025005, 9900044).isRight)
   }
 
   "LCD of 69941 and 13679" should "be 956722939" in {
-    Basics.lcm(69941, 13679) shouldBe 956722939
+    Basics.lcm(69941, 13679).left.getOrElse(0) shouldBe 956722939
   }
 
   "LCD of 0 and 0" should "throw exception" in {
-    assertThrows[Exception](Basics.lcm(0, 0))
+    assert(Basics.lcm(0, 0).isRight)
   }
 
   "GCD of 54 and 24" should "be 6" in {
